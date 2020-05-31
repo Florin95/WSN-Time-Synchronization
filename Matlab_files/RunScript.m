fdbfccdab388220901edde67d88d8dd6e8813fb7
Florin <florin.nastase@outlook.com>
2020-05-31 22:44:08 +0300

    Refactored and reorganized files

--- Matlab_files/RunScript.m
+++ Matlab_files/RunScript.m
@@ -4,14 +4,27 @@ TPSN_SYNC_WORD = hex2dec('ABABABAB');
 ALIGNMENT_WORD = hex2dec('A5A5A5A5');
 
 en_dev1 = 1;
+path1 = 'D:\Facultate\MASTER\Anul_2\Disertatie\PSoC6\Workspace\Connectivity_Secure_TCP_Client\Matlab_files\SNTP\Attempt4_SNTP_RESULT_1';
+path2 = 'D:\Facultate\MASTER\Anul_2\Disertatie\PSoC6\Workspace\Connectivity_Secure_TCP_Client\Matlab_files\SNTP\Attempt5_SNTP_25_minutes';
+path3 = 'D:\Facultate\MASTER\Anul_2\Disertatie\PSoC6\Workspace\Connectivity_Secure_TCP_Client\Matlab_files\SNTP\Attempt3_data';
 
-fileName0 = 'C:\Users\Nastase\Desktop\dev0.log';
-fileName1 = 'C:\Users\Nastase\Desktop\dev1.log';
+path4 = 'D:\Facultate\MASTER\Anul_2\Disertatie\PSoC6\Workspace\Connectivity_Secure_TCP_Client\Matlab_files\TPSN\Attempt3_TPSN_20_minutes';
+
+path = path4;
+fileName0 = strcat(path, '\dev0.log');
+fileName1 = strcat(path, '\dev1.log');
 
 [sync0, ts0, data0, seconds0] = CheckTS(fileName0);
 if (en_dev1)
     [sync1, ts1, data1, seconds1] = CheckTS(fileName1);
 end
+
+min_ts0 = min(ts0);
+min_ts1 = min(ts1);
+min_ts = min([min_ts0, min_ts1]);
+
+ts0 = ts0 - min_ts;
+ts1 = ts1 - min_ts;
 
 plot_0 = plot(ts0, data0);
 
@@ -21,25 +34,23 @@ if (en_dev1)
 end
 
 grid on;
-
-% for i=1:length(seconds0)
-%     line([seconds0(i), seconds0(i)], [0,15000], 'color', 'green', ...
-%         'LineWidth', 3);
-% end
+enable_TPSN_sync_points_mark = 0;
 
 sync0_ts = ts0(sync0 == TPSN_SYNC_WORD);
 sync1_ts = ts1(sync1 == TPSN_SYNC_WORD);
 sync_line_0 = [];
 sync_line_1 = [];
+    
+if (enable_TPSN_sync_points_mark)
+    for i=1:length(sync0_ts)
+        sync_line_0 = line([sync0_ts(i), sync0_ts(i)], [0,15000], 'color', 'green', ...
+            'LineWidth', 3);
+    end
 
-for i=1:length(sync0_ts)
-    sync_line_0 = line([sync0_ts(i), sync0_ts(i)], [0,15000], 'color', 'green', ...
-        'LineWidth', 3);
-end
-
-for i=1:length(sync1_ts)
-    sync_line_1 = line([sync1_ts(i), sync1_ts(i)], [0,15000], 'color', 'magenta', ...
-        'LineWidth', 3);
+    for i=1:length(sync1_ts)
+        sync_line_1 = line([sync1_ts(i), sync1_ts(i)], [0,15000], 'color', 'magenta', ...
+            'LineWidth', 3);
+    end
 end
 
 xlabel('Timestamp [uS]');
@@ -48,9 +59,8 @@ ylabel('Voltage [mV]');
 legend([plot_0, plot_1, sync_line_0, sync_line_1], 'Device 0', 'Device 1', 'Sync Dev 0', 'Sync Dev 1');
 
 %%
-% [x,y] = ginput(2); % click on each point
-% dx = diff(x)
+[x,y] = ginput(2); % click on each point
+dx = diff(x)
 
-% set(gca,'XTick',unique(ts_s));
-% set(gca,'XTickLabel',[4 8 15 21] );
-% set(gca,'YGrid','off','XGrid','on');
+
+
